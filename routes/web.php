5fa7c6c0fbd32dad83c36b772f2bd786dfa49f2a
Alexisb710 <alexisbm710@gmail.com>
2024-10-30 10:07:46 -0700

    added frontend files

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'home']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
