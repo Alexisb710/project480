9039a9573f91e00ce9efed567491d25247641464
Alexisb710 <alexisbm710@gmail.com>
2024-11-01 12:58:26 -0700

    updated Home controller to send all product tab;e data to product view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -11,7 +12,8 @@ class HomeController extends Controller
     }
 
     public function home(){
-        return view('home.index');
+        $products = Product::all();
+        return view('home.index', compact('products'));
     }
 
 }
